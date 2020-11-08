51f79f4342b84975f9c9a763bc925f5c3893021b
Glitch (andy-worhol-tribute) <none>
2020-11-08 14:27:25 +0000

    Version 1 - Week 3

--- script.js
+++ script.js
@@ -3,3 +3,38 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+
+// COMMUNITY.html - TABS
+function openPage(pageName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
+  document.getElementById("defaultOpen").click();
+
+
+// ABOUT.html - TABS
+function openCity(evt, cityName) {
+  var i, tabcontent2, tablinks;
+  tabcontent2 = document.getElementsByClassName("tabcontent2");
+  for (i = 0; i < tabcontent2.length; i++) {
+    tabcontent2[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
